c190b0cc9e9b08575dd1a0ac21787508eb73d70a
Shift <moneygroup102@gmail.com>
2019-08-04 20:21:38 +0500

    Initial Commit

---
+++ Solar Alarm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Solar Alarm
+//
+//  Created by Алмаз Фанилевич on 04.08.2019.
+//  Copyright © 2019 Shift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
